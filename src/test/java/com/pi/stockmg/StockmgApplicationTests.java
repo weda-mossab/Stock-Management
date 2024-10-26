97a03a81f9af68c9d2155f4dcdd1bcbc6926d27e
Mossab Weda <mossab.weda@metam.tech>
2024-10-26 14:57:38 +0100

    first commit

---
+++ src/test/java/com/pi/stockmg/StockmgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.stockmg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockmgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
