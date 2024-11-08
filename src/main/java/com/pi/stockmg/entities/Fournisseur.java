7f684656ebd01e293f1ee7de0aae47e22a25bf70
Mossab Weda <mossab.weda@metam.tech>
2024-11-08 22:15:20 +0000

    Change the id from Integer to Long

--- src/main/java/com/pi/stockmg/entities/Fournisseur.java
+++ src/main/java/com/pi/stockmg/entities/Fournisseur.java
@@ -20,7 +20,7 @@ import lombok.ToString;
 public class Fournisseur {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
     
     private String nom;
     private String adresse; 
