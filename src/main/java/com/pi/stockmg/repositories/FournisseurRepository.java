7bc91b4d85e36660d7186188d5c8a7f4e344435e
Mossab Weda <mossab.weda@metam.tech>
2024-11-08 22:16:22 +0000

    Create the supplier repo,controller and the front page

---
+++ src/main/java/com/pi/stockmg/repositories/FournisseurRepository.java
@@ -0,0 +1,8 @@
+package com.pi.stockmg.repositories;
+
+import com.pi.stockmg.entities.Fournisseur;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FournisseurRepository extends JpaRepository<Fournisseur,Long> {
+
+}
